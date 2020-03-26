35c5e741a8c836676686bc22251c6e94afbbbf7b
jean paul <58843994+Hamelet@users.noreply.github.com>
2020-03-26 15:13:34 +0100

    modif js

--- main.js
+++ main.js
@@ -1,35 +1,20 @@
-// function setup() {
-//     createCanvas(displayWidth/4, displayHeight/4);
-   
-// }
-// function draw() {
-//     background(0, 0, 255);
-//     stroke(255, 255, 0);
-//     line(0, 0, width, height);
-//     fill(255, 0, 0);
-//     circle(width/2, height/2, 20);
-// }
-// let value = 0;
-// function draw() {
-//   fill(value);
-//   rect(25, 25, 50, 50);
-// }
-// function mousePressed() {
-//   if (value === 0) {
-//     value = 255;
-//   } else {
-//     value = 0;
-//   }
-// }
-function draw() {
-    background(237, 34, 93);
-    fill(0);
-  
-    if (mouseIsPressed) {
-      ellipse(50, 50, 50, 50);
-    } else {
-      rect(25, 25, 50, 50);
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function windowResize(){
+    resizeCanvas(windowWidth, windowHeight, false);
+}
+
+function draw(){
+    // background(0, 0, 255);
+    if(mouseIsPressed)
+    {
+        if(mouseButton === RIGHT)
+        {
+            background(0, 0, 255);
+        }
+        fill(random(255),random(255), random(255));
+        circle(mouseX, mouseY, 200);
     }
-  
-    print(mouseIsPressed);
-  }
+}
